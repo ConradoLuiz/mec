57a71963a2af6ea7076cdee77f3919379675bfc3
ConradoLuiz <conradol.vargas@gmail.com>
2020-12-24 02:18:13 -0300

    condicional

--- mec.py
+++ mec.py
@@ -9,14 +9,14 @@ keywords = (
 )
 '''
 keywords = (
-    'mec', 'meczada', 'loop', 'se', 'senao', 'funcao', 'mostrar', 'voltar'
+    'mec', 'meczada', 'loop', 'se', 'senao', 'funcao', 'mostrar', 'vale', 'pprt', 'falso', 'vdd'
 )
 
 tokens = keywords + (
     'EQUALS', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'POWER',
     'LPAREN', 'RPAREN', 'LT', 'LE', 'GT', 'GE',
     'COMMA', 'SEMI', 'INTEGER', 'FLOAT', 'STRING',
-    'ID', 'NEWLINE', 'COMMENT'
+    'ID', 'NEWLINE', 'COMMENT', 'LBRACES', 'RBRACES'
 )
 
 t_ignore = ' \t'
@@ -34,7 +34,7 @@ def t_ID(t):
     return t
 
 
-t_EQUALS = r'='
+# t_EQUALS = r'(vale)'
 t_PLUS = r'\+'
 t_MINUS = r'-'
 t_TIMES = r'\*'
@@ -42,6 +42,8 @@ t_POWER = r'\^'
 t_DIVIDE = r'/'
 t_LPAREN = r'\('
 t_RPAREN = r'\)'
+t_LBRACES = r'\{'
+t_RBRACES = r'\}'
 t_LT = r'<'
 t_LE = r'<='
 t_GT = r'>'
@@ -79,49 +81,73 @@ def p_program(p):
     '''program : program statement
                | statement'''
     if len(p) == 2 and p[1]:
-        p[0] = {}
-        line, stat = p[1]
-        p[0][line] = stat
-    elif len(p) == 3:
+        # print('program : statement ->', p[0], p[1])
+        p[0] = []
         p[0] = p[1]
-        if not p[0]:
-            p[0] = {}
-        if p[2]:
-            line, stat = p[2]
-            p[0][line] = stat
+    elif len(p) == 3:
+        # print('program : program statement ->', p[0], p[1], p[2])
+        # if p[1] and not p[2]:
+        #     print('entrou so p1')
+        #     p[0] = p[1]
+
+        # if p[2] and not p[1]:
+        #     print('entrou so p2')
+        #     p[0] = p[2]
+
+        if p[1] and p[2]:
+            # print('entrou p1 e p2')
+            if type(p[1]) == tuple:
+                # print('tupla')
+                p[0] = [p[1], p[2]]
+            elif type(p[1]) == list:
+                # print('lista')
+                p[1].append(p[2])
+                p[0] = p[1]
+
+    else:
+        p[0] = []
+    # print(p[0])
+    # print('------------------------------------------------')
+    # print('------------------------------------------------')
+
+
+def p_empty(p):
+    'empty :'
+    pass
 
 
 def p_statement_assign(p):
-    "statement : mec ID EQUALS expression NEWLINE"
+    "statement : mec ID vale expression NEWLINE"
     ID = p[2]
     if ID in constants:
         raise ("Constante já declarada")
     names[ID] = p[4]
+    p[0] = ('empty',)
 
 
 def p_statement_constant(p):
-    '''statement : meczada ID EQUALS expression 
-                 | meczada ID EQUALS expression NEWLINE'''
+    '''statement : meczada ID vale expression 
+                 | meczada ID vale expression NEWLINE'''
     ID = p[2]
     if ID in constants:
         raise ("Constante já declarada")
     constants[ID] = p[4]
+    p[0] = ('empty',)
 
 
 def p_statement_reassign(p):
-    '''statement : ID EQUALS expression 
-                 | ID EQUALS expression NEWLINE'''
+    '''statement : ID vale expression 
+                 | ID vale expression NEWLINE'''
     ID = p[1]
     if ID in constants:
         raise ("Constante já declarada")
     names[ID] = p[3]
+    p[0] = ('empty',)
 
 
-def p_statement_expr(p):
-    '''statement : expression
-                 | expression NEWLINE'''
-    # print(p[1])
-    pass
+# def p_statement_expr(p):
+#     '''statement : empty'''
+#     pass
 
 
 def p_statement_comment(p):
@@ -139,27 +165,72 @@ def p_statement_command(p):
 def p_command_mostrar(p):
     '''command : mostrar LPAREN expression RPAREN
                | mostrar LPAREN expression RPAREN NEWLINE'''
-    print(p[3])
+    p[0] = ('mostrar', p[3])
 
 
 def p_expression_binop(p):
     '''expression : expression PLUS expression
                   | expression MINUS expression
                   | expression TIMES expression
-                  | expression DIVIDE expression'''
-    if p[2] == '+':
-        p[0] = p[1] + p[3]
-    elif p[2] == '-':
-        p[0] = p[1] - p[3]
-    elif p[2] == '*':
-        p[0] = p[1] * p[3]
-    elif p[2] == '/':
-        p[0] = p[1] / p[3]
+                  | expression DIVIDE expression
+                  | falso 
+                  | vdd'''
+    if len(p) == 2:
+        p[0] = True if p[1] == 'vdd' else False
+    else:
+        if p[2] == '+':
+            p[0] = p[1] + p[3]
+        elif p[2] == '-':
+            p[0] = p[1] - p[3]
+        elif p[2] == '*':
+            p[0] = p[1] * p[3]
+        elif p[2] == '/':
+            p[0] = p[1] / p[3]
 
 
 def p_expression_group(p):
     "expression : LPAREN expression RPAREN"
     p[0] = p[2]
+
+
+def p_if_statement(p):
+    '''
+    statement : se expression LBRACES program RBRACES NEWLINE
+    '''
+    if p[2]:
+        p[0] = p[4]
+    else:
+        p[0] = ('empty')
+
+
+def p_if_statement_newline(p):
+    '''
+    statement : se expression LBRACES NEWLINE program RBRACES NEWLINE
+    '''
+    if p[2]:
+        p[0] = p[5]
+    else:
+        p[0] = ('empty')
+
+
+def p_if_statement_newline_else(p):
+    '''
+    statement : se expression LBRACES NEWLINE program RBRACES senao LBRACES NEWLINE program RBRACES NEWLINE
+    '''
+    if p[2]:
+        p[0] = p[5]
+    else:
+        p[0] = p[10]
+
+
+def p_if_statement_newline_else_newline(p):
+    '''
+    statement : se expression LBRACES NEWLINE program RBRACES NEWLINE senao LBRACES NEWLINE program RBRACES NEWLINE
+    '''
+    if p[2]:
+        p[0] = p[5]
+    else:
+        p[0] = p[11]
 
 
 def p_expression_int(p):
@@ -199,6 +270,15 @@ def p_error(p):
 
 parser = yacc.yacc()
 
+
+def interp(prog):
+    for command in prog:
+        if type(command) == list:
+            interp(command)
+        elif command[0] == 'mostrar':
+            print(command[1])
+
+
 if len(sys.argv) == 2:
     if ".mec" not in sys.argv[1]:
         print("Arquivo não suportado!")
@@ -206,8 +286,9 @@ if len(sys.argv) == 2:
     with open(sys.argv[1]) as f:
         data = f.read()
     prog = parser.parse(data, debug=0)
+    print('FINAL', prog)
+    interp(prog)
     try:
-        # print('FINAL', prog)
         pass
     except:
         print("Não foi possivel criar o parser!")
